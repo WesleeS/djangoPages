86319056229bd7f01cd06339413fc7ab3ecac9c1
WesleeS <weslee.shear@umontana.edu>
2023-09-26 20:08:37 -0600

    Extra Page
    Adds an extra page, plus the header content blocks.

--- pages/tests.py
+++ pages/tests.py
@@ -26,3 +26,16 @@ class AboutpageTests(SimpleTestCase):
         self.assertTemplateUsed(response, "about.html")
     def test_template_content(self): 
         response = self.client.get(reverse("about"))
+
+class QuiltpageTests(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/quilt/")
+        self.assertEqual(response.status_code, 200)
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("quilt"))
+        self.assertEqual(response.status_code, 200)
+    def test_template_name_correct(self):
+        response = self.client.get(reverse("quilt"))
+        self.assertTemplateUsed(response, "quilt.html")
+    def test_template_content(self): 
+        response = self.client.get(reverse("quilt"))
--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, QuiltPageView
 
 urlpatterns = [
     path("about/", AboutPageView.as_view(), name="about"),
+    path("quilt/", QuiltPageView.as_view(), name="quilt"),
     path("", HomePageView.as_view(), name="home"),
 ]
